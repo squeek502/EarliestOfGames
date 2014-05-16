b9786ea91c6042663b8ba08342c162616e9ce0df
squeek <squeek502@hotmail.com>
2014-05-15 22:53:29 -0700

    15 minute autocommit (05/15/14 22:53:28)
    M  java/squeek/earliestofgames/base/Container.java

--- java/squeek/earliestofgames/base/Container.java
+++ java/squeek/earliestofgames/base/Container.java
@@ -5,6 +5,11 @@ import net.minecraft.inventory.Slot;
 
 public abstract class Container extends net.minecraft.inventory.Container
 {
+	protected void addPlayerInventorySlots(InventoryPlayer playerInventory, int yStart)
+	{
+		addPlayerInventorySlots(playerInventory, 8, yStart);
+	}
+	
 	protected void addPlayerInventorySlots(InventoryPlayer playerInventory, int xStart, int yStart)
 	{
 		// inventory
@@ -12,14 +17,14 @@ public abstract class Container extends net.minecraft.inventory.Container
         {
             for (int col = 0; col < 9; ++col)
             {
-                this.addSlotToContainer(new Slot(playerInventory, col + row * 9 + 9, 8 + col * 18, row * 18 + b0));
+                this.addSlotToContainer(new Slot(playerInventory, col + row * 9 + 9, xStart + col * 18, yStart + row * 18));
             }
         }
 
         // hotbar
         for (int col = 0; col < 9; ++col)
         {
-            this.addSlotToContainer(new Slot(playerInventory, col, 8 + col * 18, 58 + b0));
+            this.addSlotToContainer(new Slot(playerInventory, col, xStart + col * 18, yStart + 58));
         }
 	}
 }
--- java/squeek/earliestofgames/content/CrateContainer.java
+++ java/squeek/earliestofgames/content/CrateContainer.java
@@ -2,15 +2,23 @@ package squeek.earliestofgames.content;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Slot;
 import squeek.earliestofgames.base.Container;
 
 public class CrateContainer extends Container
 {
 	CrateTile crate = null;
-	
+
 	public CrateContainer(InventoryPlayer playerInventory, CrateTile crate)
 	{
 		this.crate = crate;
+
+		for (int i = 0; i < crate.getSizeInventory(); ++i)
+		{
+			this.addSlotToContainer(new Slot(crate, i, 44 + i * 18, 20));
+		}
+
+		addPlayerInventorySlots(playerInventory, 69);
 	}
 
 	@Override
@@ -18,5 +26,5 @@ public class CrateContainer extends Container
 	{
 		return false;
 	}
-	
+
 }
