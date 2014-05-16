cc711b9b7c3f7da43b4f722fe2da1ce0bac30f2a
squeek <squeek502@hotmail.com>
2014-05-15 21:08:06 -0700

    15 minute autocommit (05/15/14 21:08:06)
    M  java/squeek/earliestofgames/blocks/CrateTile.java

--- java/squeek/earliestofgames/blocks/CrateTile.java
+++ java/squeek/earliestofgames/blocks/CrateTile.java
@@ -3,6 +3,8 @@ package squeek.earliestofgames.blocks;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 
 public class CrateTile extends TileEntity implements IInventory
@@ -29,7 +31,17 @@ public class CrateTile extends TileEntity implements IInventory
 	@Override
 	public ItemStack decrStackSize(int slotNum, int count)
 	{
-		return null;
+		ItemStack itemStack = getStackInSlot(slotNum);
+
+		if (itemStack != null)
+		{
+			if (itemStack.stackSize <= count)
+				setInventorySlotContents(slotNum, null);
+			else
+				itemStack = itemStack.splitStack(count);
+		}
+
+		return itemStack;
 	}
 
 	@Override
@@ -43,6 +55,10 @@ public class CrateTile extends TileEntity implements IInventory
 	@Override
 	public void setInventorySlotContents(int slotNum, ItemStack itemStack)
 	{
+		inventoryItems[slotNum] = itemStack;
+
+		if (itemStack != null && itemStack.stackSize > getInventoryStackLimit())
+			itemStack.stackSize = getInventoryStackLimit();
 	}
 
 	@Override
@@ -85,5 +101,28 @@ public class CrateTile extends TileEntity implements IInventory
 	public boolean isItemValidForSlot(int slotNum, ItemStack itemStack)
 	{
 		return true;
+	}
+
+	@Override
+	public void writeToNBT(NBTTagCompound compound)
+	{
+		super.writeToNBT(compound);
+
+		NBTTagList items = new NBTTagList();
+
+		for (int slotNum = 0; slotNum < getSizeInventory(); slotNum++)
+		{
+			ItemStack stack = getStackInSlot(slotNum);
+
+			if (stack != null)
+			{
+				NBTTagCompound item = new NBTTagCompound();
+				item.setByte("Slot", (byte) slotNum);
+				stack.writeToNBT(item);
+				items.appendTag(item);
+			}
+		}
+
+		compound.setTag("Inventory", items);
 	}
 }
