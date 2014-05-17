8c89040ebf1593e7e4d814a933df86b0a322ae96
squeek <squeek502@hotmail.com>
2014-05-17 14:53:01 -0700

    15 minute autocommit (05/17/14 14:53:01)
    M  java/squeek/earliestofgames/asm/Hooks.java

--- java/squeek/earliestofgames/asm/Hooks.java
+++ java/squeek/earliestofgames/asm/Hooks.java
@@ -1,6 +1,8 @@
 package squeek.earliestofgames.asm;
 
+import java.lang.reflect.InvocationTargetException;
 import net.minecraft.block.BlockDynamicLiquid;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.ForgeDirection;
@@ -26,7 +28,16 @@ public class Hooks
 		if (tile != null && tile instanceof CrateTile)
 		{
 			ModEarliestOfGames.Log.info("doesFlowGetBlockedBy: "+ForgeDirection.getOrientation(flowDirection).toString());
+			return !((CrateTile) tile).canItemPassThroughSide(new ItemStack(flowingBlock, 1, 0), ForgeDirection.getOrientation(flowDirection).getOpposite());
 		}
-		return true;
+		try
+		{
+			return (Boolean) flowingBlock.getClass().getDeclaredMethod("func_149807_p", World.class, Integer.TYPE, Integer.TYPE, Integer.TYPE).invoke(flowingBlock, world, x, y, z);
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return true;
+		}
 	}
 }
