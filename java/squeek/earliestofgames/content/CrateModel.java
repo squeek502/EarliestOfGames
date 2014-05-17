dcd74195069915cb12fd817d87591b8f0b2afc50
squeek <squeek502@hotmail.com>
2014-05-17 02:34:53 -0700

    15 minute autocommit (05/17/14 02:34:53)
    M  java/squeek/earliestofgames/content/CrateModel.java

--- java/squeek/earliestofgames/content/CrateModel.java
+++ java/squeek/earliestofgames/content/CrateModel.java
@@ -9,35 +9,65 @@ import net.minecraftforge.common.util.ForgeDirection;
 
 public class CrateModel extends ModelBase
 {
-	private ModelRenderer[] frame = new ModelRenderer[ForgeDirection.VALID_DIRECTIONS.length];
+	private ModelRenderer[] frame = new ModelRenderer[ForgeDirection.VALID_DIRECTIONS.length*2];
 	private float scale = 0.0625f;
 	
 	public CrateModel()
 	{
 		int sideWidth = (int) (ModContent.blockCrate.sideWidth / scale);
 		int sideLength = (int) (1f / scale);
+		int pillarIndex = 0;
 		
 		for (ForgeDirection side : ForgeDirection.VALID_DIRECTIONS)
 		{
+			if (side == ForgeDirection.UP || side == ForgeDirection.DOWN)
+				continue;
+			
 			AxisAlignedBB sideBounds = ModContent.blockCrate.getSideBoundingBox(side);
+			int numPillars = side.offsetX != 0 ? 4 : 2;
 			
-			double originX = sideBounds.minX / scale, originY = sideBounds.minY / scale, originZ = sideBounds.minZ / scale;
-			int sizeX = (int) ((sideBounds.maxX-sideBounds.minX) / scale);
-			int sizeY = (int) ((sideBounds.maxY-sideBounds.minY) / scale);
-			int sizeZ = (int) ((sideBounds.maxZ-sideBounds.minZ) / scale);
-			
-			frame[side.ordinal()] = new ModelRenderer(this, 0, 0).setTextureSize(16, 16).setTextureOffset(0, 0);
-			frame[side.ordinal()].addBox((float) originX, (float) originY, (float) originZ, sizeX, sizeY, sizeZ);
+			for (int pillarNum = 0; pillarNum < numPillars; pillarNum++)
+			{
+				double originX = sideBounds.minX / scale, originY = sideBounds.minY / scale, originZ = sideBounds.minZ / scale;
+				int sizeX = (int) ((sideBounds.maxX-sideBounds.minX) / scale);
+				int sizeY = (int) ((sideBounds.maxY-sideBounds.minY) / scale);
+				int sizeZ = (int) ((sideBounds.maxZ-sideBounds.minZ) / scale);
+				
+				if (side.offsetX != 0)
+				{
+					if (pillarNum < 2)
+					{
+						sizeZ = sizeX;
+						originZ = originZ + pillarNum * (sideLength - sideWidth);
+					}
+					else
+					{
+						originZ += sideWidth;
+						sizeZ -= sideWidth*2;
+						originY = originY + (pillarNum-2) * (sideLength - sideWidth);
+						sizeY = sizeX;
+					}
+				}
+				if (side.offsetZ != 0)
+				{
+					originX += sideWidth;
+					sizeX -= sideWidth*2;
+					originY = originY + pillarNum * (sideLength - sideWidth);
+					sizeY = sizeZ;
+				}
+				
+				ModelRenderer pillar = new ModelRenderer(this, 0, 0).setTextureSize(16, 16);
+				pillar.addBox((float) originX, (float) originY, (float) originZ, sizeX, sizeY, sizeZ);
+				
+				frame[pillarIndex++] = pillar;
+			}
 		}
 	}
 	
 	public void renderAll()
 	{
 		for (ModelRenderer framePart : frame)
-		{
-			framePart.mirror = true;
 			framePart.render(scale);
-		}
 	}
 	
 	@Override
