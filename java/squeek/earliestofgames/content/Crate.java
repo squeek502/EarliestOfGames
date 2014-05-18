b611d0b4ff11446af44d0df975f1b5794dd3eb0d
squeek <squeek502@hotmail.com>
2014-05-18 10:54:58 -0700

    15 minute autocommit (05/18/14 10:54:58)
    M  java/squeek/earliestofgames/content/Crate.java

--- java/squeek/earliestofgames/content/Crate.java
+++ java/squeek/earliestofgames/content/Crate.java
@@ -34,7 +34,7 @@ public class Crate extends BlockContainer
 
 	public Crate()
 	{
-		super(Material.water);
+		super(crateMaterial);
 		setBlockName(ModInfo.MODID + "." + this.getClass().getSimpleName());
 		setBlockTextureName(ModInfo.MODID + ":" + this.getClass().getSimpleName());
 	}
