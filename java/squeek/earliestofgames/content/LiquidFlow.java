fc4caa716b89eabdcb38fb6f43f984344520e915
squeek <squeek502@hotmail.com>
2014-05-17 17:23:33 -0700

    15 minute autocommit (05/17/14 17:23:32)
    M  java/squeek/earliestofgames/content/LiquidFlow.java

--- java/squeek/earliestofgames/content/LiquidFlow.java
+++ java/squeek/earliestofgames/content/LiquidFlow.java
@@ -1,6 +1,37 @@
 package squeek.earliestofgames.content;
 
+import net.minecraft.block.Block;
+import net.minecraft.util.Vec3;
+import net.minecraftforge.common.util.ForgeDirection;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
+
 public class LiquidFlow
 {
-
+	public LiquidFlowInfo[] sidedLiquidFlows = new LiquidFlowInfo[ForgeDirection.VALID_DIRECTIONS.length];
+	protected Vec3 flowVector = new Vec3(0D, 0D, 0D);
+	
+	public class LiquidFlowInfo
+	{
+		public Fluid fluid;
+		
+		public LiquidFlowInfo(Fluid fluid)
+		{
+			this.fluid = fluid;
+		}
+	}
+	
+	public void onLiquidFlowFrom(Block block, int flowDecay, int flowDirection)
+	{
+		Fluid fluid = FluidRegistry.lookupFluidForBlock(block);
+		if (fluid != null)
+		{
+			sidedLiquidFlows[flowDirection] = new LiquidFlowInfo(fluid);
+		}
+	}
+	
+	public Vec3 getFlowVector()
+	{
+		
+	}
 }
